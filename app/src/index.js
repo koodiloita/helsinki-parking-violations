c5511509e56cdc764de6c5803ec2e7777ad87e83
Markus P <koodiloita@gmail.com>
2018-02-15 21:19:23 +0200

    Revert "Load Mapbox token from environment variables"
    This reverts commit f942373b30f681f4d10e17a63a48069691d800cc.

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,8 @@
-/* eslint-disable import/first */
-require('dotenv').config()
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
