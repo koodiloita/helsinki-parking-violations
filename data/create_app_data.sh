62341d6e57e2e37a3a0d7ca54f9f6837ed5205d9
Markus P <koodiloita@gmail.com>
2018-02-15 21:42:48 +0200

    Prevent crashing if target folder not exists

--- data/create_app_data.sh
+++ data/create_app_data.sh
@@ -12,4 +12,5 @@ APP_DATA_FILENAME=appData.json
 python geocode_fetcher.py $GOOGLE_API_KEY $PARKING_DATA_FILENAME $GEOCODE_FILENAME
 python coordinates_parser.py $GEOCODE_FILENAME $COORDINATES_FILENAME
 python app_data_generator.py $COORDINATES_FILENAME $PARKING_DATA_FILENAME $APP_DATA_FILENAME
+mkdir -p ../app/src/data
 cp $APP_DATA_FILENAME ../app/src/data/
